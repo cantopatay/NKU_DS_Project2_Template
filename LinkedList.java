613a90ed0ac8b4c408873d923de0cf11a437d013
cantopatay <102103206+cantopatay@users.noreply.github.com>
2022-04-16 00:30:44 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class LinkedList {
     private Node head;
 
@@ -61,21 +64,43 @@ public class LinkedList {
     // bir metod tanimlayip PrintReverse metodu icinde tanimlamis oldugunuz bu metodu kullanabilirsiniz.
 
     public void PrintReverse(){
-        
+        LinkedList tmp = new LinkedList();
+        Node n = this.head;
+        while (n!= null){tmp.Prepend(n.value); n=n.next;}
+        tmp.Print();
     }
 
-    // Bu metod bagli listedeki elemanlarin toplamini dondurur. 
-    // Eger bagli liste bossa metod 0 degerini dondurmelidir. 
+    public void Prepend(int v){
+        Node n = new Node(v);
+        n.next = this.head;
+        this.head=n;
+    }
+
+    // Bu metod bagli listedeki elemanlarin toplamini dondurur.
 
     public int Sum(){
-       
+       Node current = this.head;
+       int sum =0;
+       while (current != null){
+           sum+= current.value;
+           current=current.next;
+       }
+       return sum;
     }
 
     // IsSorted() bagli liste kucukten buyuge sirali ise true aksi durumda false dondurur.
     // Bagli liste bos ise veya tek elemanli ise IsSorted() true dondurur.
 
     public boolean IsSorted(){
-        
+        if(this.head == null || this.head.next==null) return true;
+
+        Node prev;
+        prev = this.head;
+        while(prev.next != null){
+            if(prev.value > prev.next.value) return false;
+            prev = prev.next;
+        }
+        return true;
     }
 
     // MergeSortedLists(LinkedList m, LinkedList n) parametre olarak iki tane kucukten
@@ -83,6 +108,17 @@ public class LinkedList {
     // siralandigi bir bagli liste dondurur.
 
     public static LinkedList MergeSortedLists(LinkedList m, LinkedList n){
-        
+        List<Integer> liste = new ArrayList<Integer>();
+        Node node = m.head;
+        while(node!=null){ liste.add(node.value); node=node.next; }
+        node = n.head;
+        while(node!=null){ liste.add(node.value); node=node.next; }
+        liste.sort(null); //sort ascending
+
+        LinkedList ll = new LinkedList();
+        for(int sayi : liste)ll.Append(sayi);
+
+        return ll;
+
     }
 }
